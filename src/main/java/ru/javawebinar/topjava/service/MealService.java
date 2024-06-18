2ca6823232b8b026a6bcf4c83fc2d156f03cfc47
sokolov-r <sokolov.r.s@list.ru>
2024-06-18 18:01:53 +0300

    HW2 task 4

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,42 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.web.SecurityUtil;
+
+import java.util.Collection;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFound;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 public class MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    public Meal create(Meal meal) {
+        return repository.save(meal);
+    }
+
+    public void delete(int id) {
+        checkNotFoundWithId(repository.delete(id), id);
+    }
+
+    public Meal get(int id) {
+        return checkNotFoundWithId(repository.get(id), id);
+    }
+
+    public Collection<Meal> getAll() {
+        return repository.getAll();
+    }
+
+    public void update(Meal meal) {
+        checkNotFoundWithId(repository.save(meal), SecurityUtil.authUserId());
+    }
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,38 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.web.SecurityUtil;
+
+import java.util.Collection;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 public class MealRestController {
+
     private MealService service;
 
+    public Meal create(Meal meal) {
+        checkNew(meal);
+        return service.create(meal);
+    }
+
+    public void delete(int id) {
+        service.delete(id);
+    }
+
+    public Meal get(int id) {
+        return service.get(id);
+    }
+
+    public Collection<Meal> getAll() {
+        return service.getAll();
+    }
+
+    public void update(Meal meal) {
+        service.update(meal);
+    }
 }
