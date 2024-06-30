cdae98596ced2487250d660b844fc1517988f3be
sokolov-r <sokolov.r.s@list.ru>
2024-06-30 10:36:25 +0300

    correct DB

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -32,6 +32,6 @@ CREATE TABLE meals
     date_time   TIMESTAMP NOT NULL,
     description VARCHAR   NOT NULL,
     calories    INTEGER   NOT NULL,
-    CONSTRAINT meal_dateTime_idx UNIQUE (user_id, date_time),
+    CONSTRAINT meal_user_date_idx UNIQUE (user_id, date_time),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
